a5943e585fce2e892e8f7ec5dad0c404fcc792cf
John Farrell <johnefarrell@gmail.com>
2023-06-09 11:00:35 -0400

    removing tmc2 mentions

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tmc2'`, () => {
+  it(`should have as title 'tmc'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tmc2');
+    expect(app.title).toEqual('tmc');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tmc2 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(' app is running!');
   });
 });
